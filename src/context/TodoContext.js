7fdd1419377f272362e0d04436e2bb025376f1bb
Divyanshu Prasad <90151736+Divyanshu9822@users.noreply.github.com>
2022-10-19 10:33:40 +0530

    Solved data loss on page refresh

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,14 +1,19 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const TodoContext = createContext(null);
 
 export const TodoContextProvider = ({ children }) => {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState(!localStorage.getItem("key") ? [] : JSON.parse(localStorage.getItem("key")));
+
     const [state, setState] = useState({
         todoTitle: "",
         todoDesc: "",
     });
 
+    useEffect(() => {
+        localStorage.setItem("key", JSON.stringify(todos));
+    }, [todos])
+    
     const removeTodo = (todo) => {
         setTodos(todos.filter((e) => {
             return e !== todo;
